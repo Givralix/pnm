07b85d17f42d6196821af51a2c3868ad3614d2ab
Givralix <amelie.lulia68@gmail.com>
2017-02-17 12:04:43 +0100

    dont mind me

--- src/pnm.cr
+++ src/pnm.cr
@@ -3,7 +3,7 @@ require "./pnm/*"
 module PNM
 	# Returns the file's datatype (returns nil if not supported by this module)
 	def self.datatype?(data : Array(UInt8))
-		magic_number = data[0].chr + data[1].chr
+		magic_number = data[0].chr.to_s + data[1].chr.to_s
 		dictionnary = {"P4" => "PBM", "P5" => "PGM", "P6" => "PPM", "P7" => "PAM"}
 		begin
 			dictionnary[magic_number]
